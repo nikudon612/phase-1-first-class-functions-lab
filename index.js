cbfcbc2c3b13c1def1e1f77071a797d88385b4e4
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-13 18:19:02 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+  return function fareQuintupler(fare) {
+    return fare * 5;
+  };
+}
+
+const fareDoubler = (fare) => fare * 2;
+
+const fareTripler = (fare) => fare * 3;
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+  return returnFirstTwoDrivers(drivers);
+};
